ba1d0b467beed858d47f84a357728636139a732a
hasanmehediii <mehedi200105075@gmail.com>
2025-09-30 17:04:23 +0600

    added

---
+++ src/test/java/com/mehedi/blooddonation/BloodDonationBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mehedi.blooddonation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonationBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
